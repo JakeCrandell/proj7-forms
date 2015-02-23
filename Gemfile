53fd3b8c1b2db8dd6290c1a497c9bd21b0a5cfad
JakeCrandell <crande39@msu.edu>
2015-02-23 14:18:24 -0500

    forms

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 ruby '2.1.2'
 
 gem 'sinatra', '~> 1.4.5'
-gem 'cat_api'
 gem 'holidapi'
+gem 'rack', '1.5.2'
--- app.rb
+++ app.rb
@@ -4,8 +4,13 @@ require 'holidapi'
 
 class MyWebApp < Sinatra::Base
 	get '/' do
-		@current_holidays = HolidApi.get(country: 'us', month: Time.new.month, year: Time.new.year)
-		@old_holidays = HolidApi.get(country: 'us', month: 8, year: 1994)
+
+		params['month'] ||= Time.new.month
+		params['day'] ||= Time.new.day
+		params['year'] ||= Time.new.year
+
+		@current_holidays = HolidApi.get(month: params['month'], day: params['day'], year: params['year'])
+
 		erb :index
 	end
 end
